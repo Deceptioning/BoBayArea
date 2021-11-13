4d362106cf8ba7b7519cfeafb5b271b22631d6bb
Deceptioning <Leoo.linn@gmail.com>
2021-11-12 19:13:46 -0800

    Removed API Key

--- BoBay_Area/test.py
+++ BoBay_Area/test.py
@@ -1,6 +1,6 @@
 import googlemaps
 import geocoder
 
-gmaps = googlemaps.Client(key='AIzaSyCzaTrxvJsCwF0IOIcnn52iILwxKFhhfUo')
+gmaps = googlemaps.Client(key='')
 
 geocoder.google("212 Barber Ct, Milpitas, CA 95035")
